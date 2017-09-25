760823046ac3766043554de6f040d6f25b0f63a0
DeluxeAlonso <alonso.alvarez@pucp.pe>
2017-09-24 22:00:44 -0500

    Add evaluation button

--- app/controllers/pages.js
+++ app/controllers/pages.js
@@ -1,5 +1,41 @@
 var _ = require('lodash');
+var request = require('request');
 
 exports.flight = function (req,res){
   return res.render('pages/flight', {});
+}
+
+exports.findFlightScore = function (req, res) {
+  var url = 'http://localhost:3000';
+    request({
+      hostname: 'localhost',
+      url: url,
+      method: 'GET',
+      headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+      },
+        json:{
+        	'date': req.query.date,
+    		  'departure': req.query.departure,
+    		  'origin': req.query.originAirport,
+    		  'destination': req.query.destAirport,
+    		  'flightNumber': req.query.flightNumber,
+        	'tailNumber': req.query.tailNumber,
+        	'estimatedTime': req.query.estimatedTime,
+          'temp': req.query.temp,
+          'skyCondition': req.query.skyCondition,
+          'windSpeed': req.query.windSpeed,
+          'pressure': req.query.pressure,
+          'humidity': req.query.humidity,
+          'altimeter': req.query.altimeter,
+          'rain': req.query.rain,
+          'snow': req.query.snow,
+          'fog': req.query.fog,
+          'mist': req.query.mist,
+          'freezing': req.query.freezing
+			}
+    }, function(error, response, body) {
+    	return res.ok({score: response.body.score}, 'Flight processed');
+    });
 }
--- app/routes/pages.js
+++ app/routes/pages.js
@@ -4,5 +4,6 @@ var router = express.Router();
 var controller = require('../controllers/pages');
 
 router.get('/', controller.flight);
+router.get('/flight_score', controller.findFlightScore);
 
 module.exports = router;
